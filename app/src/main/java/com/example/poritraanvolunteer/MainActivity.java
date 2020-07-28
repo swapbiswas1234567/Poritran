e179a4d0bba44a403a6d1350c3de868c4609b797
Aadeeb <aadeeb1896@gmail.com>
2020-07-28 23:29:15 +0600

    Manifest file updated

--- app/src/main/java/com/example/poritraanvolunteer/MainActivity.java
+++ app/src/main/java/com/example/poritraanvolunteer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.poritraanvolunteer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -32,9 +33,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
 
 
         progressBar=findViewById(R.id.progressBar);
